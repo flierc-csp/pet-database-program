abc8c45869ebf92645fa4f0d8379d2d375979132
cdf61 <cdf61@DESKTOP-JF1BTQK>
2021-11-04 21:39:56 -0500

    Create Pet Class

---
+++ pet-database-program/src/main/java/com/caseyflier/pet/database/program/Pet.java
@@ -0,0 +1,47 @@
+package com.caseyflier.pet.database.program;
+
+/**
+ *
+ * @author Casey Flier
+ * CSC415
+ * Assignment 1 - Part 2
+ * 2021-11-07
+ */
+public class Pet {
+    
+    private int id;
+    private String name;
+    private int age;
+    
+    public Pet(String name, int age){
+    
+        //I need to update this when Database is implemented
+        this.id = 0;
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+}
